17a9a2016c6dbda31af65d3f9f80780827ed2cae
Rishabh sati <rishabhsati6505@gmail.com>
2023-09-15 23:12:38 +0530

    Day 7

---
+++ Day 7/TowerOfHanoi.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class TowerOfHanoi {
+    static void towerOfHanoi(int n , char source , char destination , char temp){
+        if(n==0) return;
+        towerOfHanoi(n-1,source,temp,destination);
+        System.out.println("Moving ring " + n + " from " + source + " to " + destination);
+        towerOfHanoi(n-1,temp,destination,source);
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        towerOfHanoi(n,'A','B','C');
+    }
+}
