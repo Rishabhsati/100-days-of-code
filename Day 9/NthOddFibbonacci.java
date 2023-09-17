c7a2ae9b7e9715a0997befdea7f911894fa5c84a
Rishabh sati <rishabhsati6505@gmail.com>
2023-09-17 17:51:57 +0530

    Day 9

---
+++ Day 9/NthOddFibbonacci.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class NthOddFibbonacci{
+    static int solve(int n){
+        if(n==1)
+            return 0;
+        if(n==2)
+            return 1;
+        return solve(n-1) + solve(n-2);
+    }
+    public static int NthOddFibb(int n) {
+        //Write code here
+        n=(3*n+1)/2;
+        int solve = solve(n);
+        return solve;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n;
+        n = sc.nextInt();
+        System.out.println(NthOddFibb(n));
+    }
+}
